a89aa708f209c81ad0b44f2790607f8dc2b030a4
Mahmadsho <mahmadshom1712@yandex.ru>
2026-01-06 00:15:13 +0500

    newCo,,

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val settingsButtonBackId = findViewById<Button>(R.id.settings_button_back)
+    settingsButtonBackId.setOnClickListener {
+        val settingsButtonBackIntent = Intent(this@SettingsActivity, MainActivity::class.java)
+        startActivity(settingsButtonBackIntent)
+    }
+    }
+
+}
